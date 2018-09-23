109aa6921b900708f32717496c26d73938afd8bf
Kary <starkary70@gmail.com>
2018-09-23 15:08:16 +0300

    lover

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+   const SpichoneesinTriangles = [];
+    for (let i = 0; i < preferences.length; ++i) 
+        {
+            if (SpichoneesinTriangles.indexOf(i) !== -1) {
+                continue;
+            }
+            let loverone = preferences[i] - 1;
+            if (loverone === i) {
+                continue;
+            }
+            let lovertwo = preferences[loverone] - 1;
+            let loverthree = preferences[lovertwo] - 1;
+            if (loverthree ===i) {
+             SpichoneesinTriangles.push(i, loverone, lovertwo);
+                ++count;
+            }   
+        }
+    return count;
 };
